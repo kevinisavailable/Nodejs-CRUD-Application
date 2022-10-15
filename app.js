5b2238b677578017c8d9cc1693a3d11d215e9411
Aravind Roy <aravindroy1709@gmail.com>
2022-10-15 11:23:26 +0530

    Class Circle |  Learning Exports

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    res.write("Hello World")
-    res.end()
-})
+const Circle = require('./area')
+const circle = new Circle()
+console.log(circle.area(5))
+console.log(circle.circumference(4))
 
-server.listen(8000)
+// const http = require('http')
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.end(JSON.stringify({
+//     data: 'Hello World!'
+//   }))
+// });
+
+// server.listen(8000)
