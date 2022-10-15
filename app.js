870f550b4cfb383299cca7632d26fb369def1aba
Aravind Roy <aravindroy1709@gmail.com>
2022-10-15 13:20:56 +0530

    Routes are defined.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const PORT = 3000
+const http = require("http");
+const PORT = 3000;
 
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-    data: 'Hello World!'
-  }))
-});
-console.log(`Server is running on http://localhost:${PORT}`)
-server.listen(PORT)
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Hello World!",
+      })
+    );
+  }
+  if(req.url === '/about'){
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "About Page",
+      })
+    );
+  }
+  else{
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Page Not Found",
+      })
+    );
+  }
+  }
+);
+console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT);
