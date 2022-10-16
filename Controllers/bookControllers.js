796caf69dde1c90220fce4bae3f9bc88e3b67831
Aravind Roy <aravindroy1709@gmail.com>
2022-10-16 22:20:35 +0530

    Created Schema | Changed Controllers to Mongoose Functions

--- Controllers/bookControllers.js
+++ Controllers/bookControllers.js
@@ -1,9 +1,10 @@
-const connect = require('./../database/db')
 const { ObjectId } = require('mongodb')
+const Book = require('./../Models/bookModel')
 
 exports.index = async(req,res)=>{
-    const db = await connect()
-    const books =await db.collection('book').find().toArray()
+    // const db = await connect()
+    // const books =await db.collection('book').find().toArray()
+    const books = await Book.find()
     res.send(books)}
 
 exports.create = async(req,res)=>{
@@ -14,21 +15,24 @@ exports.create = async(req,res)=>{
 exports.book = async(req,res)=>{
     const _id = ObjectId(req.params.id)
     // const id = req.params.id
-    const db = await connect()
-    const book = await db.collection('book').find({_id}).toArray()
+    // const db = await connect()
+    // const book = await db.collection('book').find({_id}).toArray()
+    const book = await Book.find(_id)
     res.json(book)
 }
 
 exports.updateBook = async (req,res)=>{
     const _id = ObjectId(req.params.id)
-    const db = await connect()
-    const book = await db.collection('book').updateOne({_id},{$set: req.body})
+    await Book.updateOne({_id},{$set: req.body})
+    // const db = await connect()
+    // const book = await db.collection('book').updateOne({_id},{$set: req.body})
     res.status(200).send(`Book is Updated`)
 }
 
 exports.deleteBook = async (req,res)=>{
     const _id = ObjectId(req.params.id)
-    const db = await connect()
-    const book = await db.collection('book').deleteOne({_id})
+    // const db = await connect()
+    // const book = await db.collection('book').deleteOne({_id})
+    await Book.deleteOne({_id})
     res.status(204).send()
 }
