a05ee4e96a4a8d4846447cff9e2562bf941d1504
Aravind Roy <aravindroy1709@gmail.com>
2022-10-16 23:55:26 +0530

    Installed bcrypt for password hashing

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,24 @@
 const User = require('./../Models/userModel')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 exports.signUp = async(req,res)=>{
-    const user = await User.create(req.body)
-    res.json(req.body)
+    const password = await bcrypt.hash(req.body.password,saltRounds)
+    const data = {...req.body,password}
+    const user = await User.create(data)
+    res.json({user})
+}
+
+exports.login = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+  
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
 }
