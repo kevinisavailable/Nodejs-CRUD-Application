e3423adfe7e696cdd8f7f40859f5008b855d72ff
Aravind Roy <aravindroy1709@gmail.com>
2022-10-16 20:27:36 +0530

    Connected Mongoose | Created bookModels for defining Schema

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT | 3001
 var bodyParser = require('body-parser')
-
+const connectDB = require('./database/db')
 app.set('view engine', 'ejs');
 app.use(bodyParser.json())
 
 const router = require('./routes/index')
 app.use(router)
 
-
-app.listen(PORT,()=>{
-    console.log(`Server Started on http://localhost:${PORT}`)
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server Started on http://localhost:${PORT}`)
+    })
 })
