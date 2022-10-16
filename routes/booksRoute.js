bc0ba3267f2cb39ad9cbc3027ba1aaa6602e9ebd
Aravind Roy <aravindroy1709@gmail.com>
2022-10-16 13:57:30 +0530

    Updated booksRoute endpoint for patch and delete

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -3,14 +3,20 @@ const routerBook = express.Router()
 
 
 routerBook
-.route('/')
-.get((req,res)=>{ res.send("All books")})
-.post((req,res)=>{res.json({data:"Book is Stored"})})
+    .route('/')
+    .get((req,res)=>{ res.send("All books")})
+    .post((req,res)=>{res.json({data:"Book is Stored"})})
 
-
-routerBook.get('/:id',(req,res)=>{
-res.send(`Got a single book of id ${req.params.id}`)
-})
-
+routerBook
+    .route('/:id')
+    .get((req,res)=>{
+        res.send(`Got a single book of id ${req.params.id}`)
+    })
+    .patch((req,res)=>{
+        res.send(`Updated a single book of id ${req.params.id}`)
+    })
+    .delete((req,res)=>{
+        res.send(`Deleted a single book of id ${req.params.id}`)
+    })
 
 module.exports = routerBook
